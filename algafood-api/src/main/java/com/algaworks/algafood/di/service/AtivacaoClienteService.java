aaab8670d1ec411f999b924700656331bcb6d389
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-12-31 19:11:01 -0300

    feat: using @Configuration and @Bean

--- algafood-api/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ algafood-api/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -1,19 +1,14 @@
 package com.algaworks.algafood.di.service;
 
-import org.springframework.stereotype.Component;
-
 import com.algaworks.algafood.di.modelo.Cliente;
 import com.algaworks.algafood.notificacao.Notificador;
 
-@Component
 public class AtivacaoClienteService {
 	
 	private Notificador notificador;
 	
 	public AtivacaoClienteService(Notificador notificacao) {
 		this.notificador = notificacao;
-		
-		System.out.println("AtivacaoClienteService: " + notificacao);
 	}
 	
 	public void ativar(Cliente cliente) {
--- algafood-api/src/main/java/com/algaworks/algafood/notificacao/NotificacaoEmail.java
+++ algafood-api/src/main/java/com/algaworks/algafood/notificacao/NotificacaoEmail.java
@@ -4,16 +4,12 @@ import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.di.modelo.Cliente;
 
-@Component
+@Component	
 public class NotificacaoEmail implements Notificador {
-	
-	public NotificacaoEmail() {
-		System.out.println("NotificacaoEmail");
-	}
-		
+
 	@Override
-	public void notificar(Cliente cliente, String mensagem) {
-		System.out.printf("Notificando %s através do endereço %s: %s\n", 
+	public void notificar(Cliente cliente, String mensagem) {		
+		System.out.printf("Notificando %s através do e-mail %s: %s\n", 
 				cliente.getNome(), cliente.getEmail(), mensagem);
 	}
 
