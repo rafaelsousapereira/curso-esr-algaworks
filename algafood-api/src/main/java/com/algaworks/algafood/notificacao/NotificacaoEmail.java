06a2c65b99ce6520fa77dc3849539fc21d034231
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2025-01-02 14:39:52 -0300

    feat: Accessing the properties with configuration properties annotations

--- algafood-api/src/main/java/com/algaworks/algafood/notificacao/NotificacaoEmail.java
+++ algafood-api/src/main/java/com/algaworks/algafood/notificacao/NotificacaoEmail.java
@@ -1,5 +1,5 @@
 package com.algaworks.algafood.notificacao;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -10,16 +10,13 @@ import com.algaworks.algafood.di.modelo.Cliente;
 @Component
 public class NotificacaoEmail implements Notificador {
 	
-	@Value("${notificador.email.host-servidor}")
-	private String host;
-	
-	@Value("${notificador.email.porta-servidor}")
-	private String port;
+	@Autowired
+	private NotificacaoProperties properties;
 
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
-		System.out.println("Host: " + this.host);
-		System.out.println("Port: " + this.port);
+		System.out.println("Host: " + this.properties.getHostServidor());
+		System.out.println("Port: " + this.properties.getPortaServidor());
 		
 		System.out.printf("Notificando %s através do e-mail %s: %s\n", 
 				cliente.getNome(), cliente.getEmail(), mensagem);
