33d60a033aa27494d977422f65fc704be34b4018
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2025-01-01 23:30:35 -0300

    feat: changing application behavior with Spring Profiles

--- algafood-api/src/main/java/com/algaworks/algafood/notificacao/NotificacaoEmail.java
+++ algafood-api/src/main/java/com/algaworks/algafood/notificacao/NotificacaoEmail.java
@@ -1,11 +1,17 @@
 package com.algaworks.algafood.notificacao;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.di.modelo.Cliente;
 
+@Profile("prod")
 @TipoDeNotificacao(NivelUrgencia.COM_PRIORIDADE)
 @Component
 public class NotificacaoEmail implements Notificador {
+	
+	public NotificacaoEmail() {
+		System.out.println("Notificação REAL");
+	}
 
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {		
