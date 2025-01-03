298837e4fb6d910efaaa3c12f17ce025d86dfa29
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2025-01-03 18:21:55 -0300

    feat: add configurations jpa and add entities

---
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -0,0 +1,54 @@
+package com.algaworks.algafood.domain.model;
+
+import java.util.Objects;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tab_cozinha")
+public class Cozinha {
+
+	@Id
+	private UUID id;
+
+	@Column(name = "nome_cozinha")
+	private String nome;
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cozinha other = (Cozinha) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
