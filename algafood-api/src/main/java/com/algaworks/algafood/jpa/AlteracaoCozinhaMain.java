e0aea17c61486dbe8c056a262bed4583742f60b9
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2025-01-16 07:26:28 -0300

    feat: add kitchen update

--- algafood-api/src/main/java/com/algaworks/algafood/jpa/AlteracaoCozinhaMain.java
+++ algafood-api/src/main/java/com/algaworks/algafood/jpa/AlteracaoCozinhaMain.java
@@ -7,26 +7,19 @@ import org.springframework.context.ConfigurableApplicationContext;
 import com.algaworks.algafood.AlgafoodApiApplication;
 import com.algaworks.algafood.domain.model.Cozinha;
 
-public class InclusaoCozinhaMain {
+public class AlteracaoCozinhaMain {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
-				.web(WebApplicationType.NONE)
-				.run(args);
-				
+				.web(WebApplicationType.NONE).run(args);
+
 		CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
-		
-		Cozinha cozinha1 = new Cozinha();
-		cozinha1.setNome("Brasileira");
-		
-		Cozinha cozinha2 = new Cozinha();
-		cozinha2.setNome("Italiana");
-		
-		cozinha1 = cadastroCozinha.salvar(cozinha1);
-		cozinha2 = cadastroCozinha.salvar(cozinha2);
-		
-		System.out.printf("%d - %s\n", cozinha1.getId(), cozinha1.getNome());
-		System.out.printf("%d - %s\n", cozinha2.getId(), cozinha2.getNome());
+
+		Cozinha cozinha = new Cozinha();
+		cozinha.setId(1L);
+		cozinha.setNome("Brasileira");
+
+		cadastroCozinha.salvar(cozinha);
 	}
 
 }
