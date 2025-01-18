44dbdc53565e8d3a8d0149e662b988f5908f6e4a
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2025-01-18 18:02:34 -0300

    fix: add column name to tax

--- algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -2,6 +2,7 @@ package com.algaworks.algafood.domain.model;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -21,6 +22,8 @@ public class Restaurante {
 	private Long id;
 
 	private String nome;
+	
+	@Column(name = "taxa_frete")
 	private BigDecimal taxaFrete;
 
 }
