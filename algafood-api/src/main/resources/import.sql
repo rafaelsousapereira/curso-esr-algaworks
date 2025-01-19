6084f7b8fedabe502c9924eb9fb2ad697d27ffab
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2025-01-19 12:29:56 -0300

    feat: add new restaurants

--- algafood-api/src/main/resources/import.sql
+++ algafood-api/src/main/resources/import.sql
@@ -5,3 +5,6 @@ insert into cozinha (nome) values ('Arábe');
 -- Inserção Restaurante
 insert into restaurante (nome, taxa_frete) values ('Viet Roll', 9.90);
 insert into restaurante (nome, taxa_frete) values ('Shabat Food', 5.90);
+insert into restaurante (nome, taxa_frete) values ('Thai Gourmet', 10);
+insert into restaurante (nome, taxa_frete) values ('Thai Delivery', 9.50);
+insert into restaurante (nome, taxa_frete) values ('Tuk Tuk Comida Indiana', 15);
