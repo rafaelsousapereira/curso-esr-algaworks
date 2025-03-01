65924ab4e50d39cd11a28698126b88f2ed9aee10
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2025-03-01 13:43:14 -0300

    feat: implement query test

--- algafood-api/src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
@@ -7,6 +7,7 @@ import com.algaworks.algafood.domain.model.Cozinha;
 public interface CozinhaRepository {
 	
 	List<Cozinha> listarTodas();
+	List<Cozinha> consultarPorNome(String nome);
 	Cozinha buscarPorId(Long id);
 	Cozinha salvar(Cozinha cozinha);
 	void remover(Long id);
--- algafood-api/src/main/java/com/algaworks/algafood/infrastructure/repository/CozinhaRepositoryImpl.java
+++ algafood-api/src/main/java/com/algaworks/algafood/infrastructure/repository/CozinhaRepositoryImpl.java
@@ -7,44 +7,51 @@ import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 
-@Component
+@Repository
 public class CozinhaRepositoryImpl implements CozinhaRepository {
 
 	@PersistenceContext
 	private EntityManager manager;
-	
+
 	@Override
 	public List<Cozinha> listarTodas() {
-		return this.manager.createQuery("from Cozinha", Cozinha.class)
-				.getResultList();
+		return this.manager.createQuery("from Cozinha", Cozinha.class).getResultList();
 	}
-	
+
 	@Override
 	public Cozinha buscarPorId(Long id) {
 		return manager.find(Cozinha.class, id);
 	}
-	
+
 	@Transactional
 	@Override
 	public Cozinha salvar(Cozinha cozinha) {
 		return this.manager.merge(cozinha);
 	}
-	
+
 	@Transactional
 	@Override
 	public void remover(Long cozinhaId) {
 		Cozinha cozinha = buscarPorId(cozinhaId);
-		
+
 		if (cozinha == null) {
 			throw new EmptyResultDataAccessException(1);
 		}
-		
+
 		this.manager.remove(cozinha);
+	}
+
+	@Override
+	public List<Cozinha> consultarPorNome(String nome) {
+		return this.manager
+				.createQuery("from Cozinha where nome like :nome", Cozinha.class)
+				.setParameter("nome", "%" + nome + "%")
+				.getResultList();
 	}
 
 }
