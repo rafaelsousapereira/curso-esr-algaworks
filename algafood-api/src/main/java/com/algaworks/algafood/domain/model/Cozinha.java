8f33aa68b1b20738743bb236138acd2796e32c04
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2025-03-29 12:51:47 -0300

    fix: add one to many annotations

--- algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -1,11 +1,15 @@
 package com.algaworks.algafood.domain.model;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonRootName;
 
 import lombok.Data;
@@ -25,4 +29,7 @@ public class Cozinha {
 	@Column(nullable = false)
 	private String nome;
 
+	@JsonIgnore
+	@OneToMany(mappedBy = "cozinha")
+	private List<Restaurante> restaurantes;
 }
